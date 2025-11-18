85fa7cf8d9ef48b4e1e7d5fe0db9be3a2789e4da
Fabio Teixeira <fabio1079@gmail.com>
2025-11-18 12:38:36 -0300

    makes hidden files visible by default

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,8 +5,12 @@ return {
   priority = 1000,
   config = function()
     require("catppuccin").setup({
-      flavour = "mocha", -- latte, frappe, macchiato, mocha
-      integrations = { treesitter = true, native_lsp = true }
+      flavour = "mocha",
+      integrations = {
+        treesitter = true,
+        native_lsp = true,
+        neo_tree = true,
+      },
     })
     vim.cmd.colorscheme("catppuccin")
   end,
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -8,10 +8,26 @@ return {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons",
       "MunifTanjim/nui.nvim",
+      "catppuccin/nvim",
     },
     keys = {
       { "<leader>e", ":Neotree toggle<CR>", desc = "Toggle file tree" },
     },
+    config = function()
+      require("neo-tree").setup({
+        filesystem = {
+          filtered_items = {
+            visible = true, -- makes hidden files visible by default
+            hide_dotfiles = false,
+            hide_gitignored = false,
+          },
+          follow_current_file = { enabled = true },
+          use_libuv_file_watcher = true,
+        },
+      })
+      -- Set highlight for dimmed dotfiles after setup
+      vim.api.nvim_set_hl(0, "NeoTreeDimText", { fg = "#6c7086", italic = true })
+    end,
   },
 
   -- Status line
